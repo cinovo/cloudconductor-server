8fd3cea553bbb3bf62ceb050d4814717f8481380
Philipp Sigloch <philipp.sigloch@tullius-walden.com>
2014-11-06 14:40:45 +0100

    moves cloudinit scripts to new version

--- cloud-init/clean.sh
+++ cloud-init/clean.sh
@@ -4,6 +4,6 @@
 #
 # curl https://raw.githubusercontent.com/cinovo/cloudconductor-server/v2.13/cloud-init/clean.sh | bash
 
-rpm -ivh http://yum.cloudconductor.net/cloudconductor-2.13-1.noarch.rpm
+rpm -ivh http://yum.cloudconductor.net/cloudconductor-2.14-1.noarch.rpm
 
 service cloudconductor start
