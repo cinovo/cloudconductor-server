85e27c2b1dc7e85b37e30239a10b30ef1675da4b
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2015-09-13 17:15:58 +0200

    only run gulp/bower/npm on unix machines
    These tools cannot be called from within ant on Windows because Java cannot execute bat or cmd files with Process.exec

--- src/main/webapp/gulp/build.js
+++ src/main/webapp/gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'webapp',
+      module: 'cloudconductor',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));
