2cb535725322b44d1ec46ba879e2031eb43cdf76
kaliDETW <it.kai.liu@gmail.com>
2015-11-09 22:15:55 +0100

    config view update - UI only
    + table view
    + design after packages-servers
    + delete button
    + add button
    + dropdown from cc
    + save config
    - APIs missing, no functinlity, only UI

--- src/main/webapp/src/app/config/config.controller.js
+++ src/main/webapp/src/app/config/config.controller.js
@@ -1,21 +1,43 @@
-//(function() {
-//	'use strict';
-//
-//	angular.module('cloudconductor')
-//	.controller('ConfigController', ['$scope', '$routeParams', function($scope, $routeParams) {
-//	    $scope.templateid = $routeParams.templateid;
-//	}]);
-//
-//})();
 'use strict';
 
 angular.module('cloudconductor')
-.controller('ConfigController', [ '$scope', '$http', function(scope, http) {
-	var ctrl = this;
-	http.get('/api/config/').then(function(res) {
-		console.log(res);
-		ctrl.list = res.data;
-	}, function(err) {
-		console.log(err);
-	});
+.controller('ConfigController', [ '$scope', '$http', '$template', 'alertService', '$log', function(scope, http, template, alert, log) {
+    var ctrl = this;
+
+    log.info(template.name);
+    ctrl.templateName = template;
+
+    ctrl.load = function() {
+        http.get('/api/config/123').then(function(res) {
+        //  console.log(res);
+            ctrl.configValues = angular.fromJson(res.data);
+        }, function(err) {
+            console.log(err);
+        });
+    };
+
+    ctrl.load();
+
+    ctrl.saveChanges = function () {
+        log.info(ctrl.configValues);
+        // format to JSON
+//      http.put('/api/config/' + ctrl.templateName, ctrl.configValues).then(function(res) {
+        http.put('/api/config/GLOBAL', ctrl.configValues).then(function(res) {
+            alert.success('The configuration settings for template '+ templateId +' has been saved successfully.');
+        }, function(err) {
+            alert.error('Something went wrong when trying to save the configuration for template '+ templateId +'!');
+            console.log(err);
+        });
+    }
+
+    // add a key to the variable array. Use saveChanges() to save the changes
+    ctrl.addKey = function(key, value) {
+        ctrl.configValues[key] = value;
+    }
+
+    // delete from settings array; Use saveChanges() to save the changes
+    ctrl.deleteKey = function ( key ) {
+        log.info(key);
+        delete ctrl.configValues[key]
+    };
 }]);
