31d3f44fd27c64f86917a4bafec5844f59960311
Marc Weise <marc.weise@cinovo.de>
2017-11-23 10:26:29 +0100

    Only allow user to change his own password.

--- src/main/java/de/cinovo/cloudconductor/server/handler/UserHandler.java
+++ src/main/java/de/cinovo/cloudconductor/server/handler/UserHandler.java
@@ -1,17 +1,18 @@
 package de.cinovo.cloudconductor.server.handler;
 
-import de.cinovo.cloudconductor.api.model.User;
-import de.cinovo.cloudconductor.server.dao.IUserDAO;
-import de.cinovo.cloudconductor.server.dao.IUserGroupDAO;
-import de.cinovo.cloudconductor.server.security.TokenHandler;
-import de.cinovo.cloudconductor.server.model.EUser;
-import de.cinovo.cloudconductor.server.model.EUserGroup;
-import de.taimos.dvalin.jaxrs.security.HashedPassword;
+import java.util.HashSet;
+
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
+import de.cinovo.cloudconductor.api.model.User;
+import de.cinovo.cloudconductor.server.dao.IUserDAO;
+import de.cinovo.cloudconductor.server.dao.IUserGroupDAO;
+import de.cinovo.cloudconductor.server.model.EUser;
+import de.cinovo.cloudconductor.server.model.EUserGroup;
+import de.cinovo.cloudconductor.server.security.TokenHandler;
+import de.taimos.dvalin.jaxrs.security.HashedPassword;
 
 /**
  * Copyright 2017 Cinovo AG<br>
@@ -21,14 +22,15 @@ import java.util.HashSet;
  */
 @Service
 public class UserHandler {
-
+	
 	@Autowired
 	private IUserDAO userDAO;
 	@Autowired
 	private TokenHandler tokenHandler;
 	@Autowired
 	private IUserGroupDAO userGroupDAO;
-
+	
+	
 	/**
 	 * @param user the user to create
 	 * @return the created user
@@ -39,21 +41,21 @@ public class UserHandler {
 		eUser.setDisplayName(user.getDisplayName());
 		eUser.setEmail(user.getEmail());
 		eUser.setLoginName(user.getLoginName());
-		if(user.getPassword() != null && !user.getPassword().isEmpty()) {
+		if ((user.getPassword() != null) && !user.getPassword().isEmpty()) {
 			eUser.setPassword(new HashedPassword(user.getPassword()));
 		}
 		eUser.setRegistrationDate(DateTime.now());
-		for(String groupName : user.getUserGroups()) {
+		for (String groupName : user.getUserGroups()) {
 			EUserGroup group = this.userGroupDAO.findByName(groupName);
-			if(group != null) {
+			if (group != null) {
 				eUser.getUserGroup().add(group);
 			}
 		}
 		return this.userDAO.save(eUser);
 	}
-
+	
 	/**
-	 * @param user  the user
+	 * @param user the user
 	 * @param eUser the entity
 	 * @return the updated entity
 	 */
@@ -63,23 +65,26 @@ public class UserHandler {
 		eUser.setEmail(user.getEmail());
 		eUser.setLoginName(user.getLoginName());
 		eUser.setUserGroup(new HashSet<>());
-		for(String groupName : user.getUserGroups()) {
+		if ((user.getPassword() != null) && !user.getPassword().isEmpty()) {
+			eUser.setPassword(new HashedPassword(user.getPassword()));
+		}
+		for (String groupName : user.getUserGroups()) {
 			EUserGroup group = this.userGroupDAO.findByName(groupName);
-			if(group != null) {
+			if (group != null) {
 				eUser.getUserGroup().add(group);
 			}
 		}
 		return this.userDAO.save(eUser);
 	}
-
+	
 	/**
-	 * @param eUser       the user
+	 * @param eUser the user
 	 * @param oldPassword the old password
 	 * @param newPassword the new password
 	 * @return whether pwchange was successfull or not
 	 */
 	public boolean changePassword(EUser eUser, String oldPassword, String newPassword) {
-		if(eUser.getPassword().validate(oldPassword)) {
+		if (eUser.getPassword().validate(oldPassword)) {
 			return false;
 		}
 		eUser.setPassword(new HashedPassword(newPassword));
--- src/main/java/de/cinovo/cloudconductor/server/rest/ui/UserImpl.java
+++ src/main/java/de/cinovo/cloudconductor/server/rest/ui/UserImpl.java
@@ -1,18 +1,22 @@
 package de.cinovo.cloudconductor.server.rest.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import de.cinovo.cloudconductor.api.interfaces.IUser;
 import de.cinovo.cloudconductor.api.model.PasswordChange;
 import de.cinovo.cloudconductor.api.model.User;
 import de.cinovo.cloudconductor.server.dao.IUserDAO;
 import de.cinovo.cloudconductor.server.handler.UserHandler;
-import de.cinovo.cloudconductor.server.security.TokenHandler;
 import de.cinovo.cloudconductor.server.model.EUser;
+import de.cinovo.cloudconductor.server.security.AuthHandler;
+import de.cinovo.cloudconductor.server.security.TokenHandler;
 import de.taimos.dvalin.jaxrs.JaxRsComponent;
+import de.taimos.dvalin.jaxrs.providers.AuthorizationProvider;
+import de.taimos.dvalin.jaxrs.security.annotation.LoggedIn;
 import de.taimos.restutils.RESTAssert;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Copyright 2017 Cinovo AG<br>
@@ -22,35 +26,39 @@ import java.util.List;
  */
 @JaxRsComponent
 public class UserImpl implements IUser {
+	
 	@Autowired
 	private IUserDAO userDAO;
-
+	
 	@Autowired
 	private UserHandler userHandler;
 	@Autowired
 	private TokenHandler tokenHandler;
-
+	@Autowired
+	private AuthHandler authHandler;
+	
+	
 	@Override
 	public List<User> getUsers() {
 		List<User> result = new ArrayList<>();
-		for(EUser eUser : this.userDAO.findList()) {
+		for (EUser eUser : this.userDAO.findList()) {
 			result.add(eUser.toApi());
 		}
 		return result;
 	}
-
+	
 	@Override
 	public void save(User user) {
 		RESTAssert.assertNotNull(user);
 		RESTAssert.assertNotEmpty(user.getLoginName());
 		EUser eUser = this.userDAO.findByLoginName(user.getLoginName());
-		if(eUser == null) {
+		if (eUser == null) {
 			this.userHandler.createEntity(user);
 		} else {
 			this.userHandler.updateEntity(user, eUser);
 		}
 	}
-
+	
 	@Override
 	public void delete(String userName) {
 		RESTAssert.assertNotEmpty(userName);
@@ -58,7 +66,7 @@ public class UserImpl implements IUser {
 		RESTAssert.assertNotNull(eUser);
 		this.userDAO.delete(eUser);
 	}
-
+	
 	@Override
 	public User createAuthToken(String userName) {
 		RESTAssert.assertNotEmpty(userName);
@@ -68,28 +76,33 @@ public class UserImpl implements IUser {
 		eUser = this.userDAO.findByLoginName(userName);
 		return eUser.toApi();
 	}
-
+	
 	@Override
 	public User revokeAuthToken(String userName, String token) {
 		RESTAssert.assertNotEmpty(userName);
 		RESTAssert.assertNotEmpty(token);
 		EUser eUser = this.userDAO.findByLoginName(userName);
 		RESTAssert.assertNotNull(eUser);
-		if(this.tokenHandler.revokeAuthToken(eUser, token)) {
+		if (this.tokenHandler.revokeAuthToken(eUser, token)) {
 			eUser = this.userDAO.findByLoginName(userName);
 		}
 		return eUser.toApi();
 	}
-
+	
 	@Override
+	@LoggedIn
 	public void changePassword(PasswordChange pwChange) {
 		RESTAssert.assertNotNull(pwChange);
 		RESTAssert.assertNotEmpty(pwChange.getUserName());
+		RESTAssert.assertFalse(pwChange.getUserName().equalsIgnoreCase(AuthorizationProvider.ANONYMOUS_USER));
 		RESTAssert.assertNotEmpty(pwChange.getNewPassword());
 		RESTAssert.assertNotEmpty(pwChange.getOldPassword());
+		RESTAssert.assertFalse(pwChange.getNewPassword().equals(pwChange.getOldPassword()));
+		EUser currentUser = this.authHandler.getCurrentUser();
+		RESTAssert.assertNotNull(currentUser);
+		RESTAssert.assertTrue(pwChange.getUserName().equals(currentUser.getUsername()));
 		EUser eUser = this.userDAO.findByLoginName(pwChange.getUserName());
 		RESTAssert.assertNotNull(eUser);
 		this.userHandler.changePassword(eUser, pwChange.getOldPassword(), pwChange.getNewPassword());
 	}
-
 }
