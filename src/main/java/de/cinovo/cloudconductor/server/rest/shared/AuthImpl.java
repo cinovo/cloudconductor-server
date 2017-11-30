d41f8e19ea25251a23ad6bf425b261ccfd63f146
Marc Weise <marc.weise@cinovo.de>
2017-11-30 14:24:54 +0100

    Implement interface for refreshing JWTs.

--- src/main/java/de/cinovo/cloudconductor/server/rest/shared/AuthImpl.java
+++ src/main/java/de/cinovo/cloudconductor/server/rest/shared/AuthImpl.java
@@ -1,5 +1,11 @@
 package de.cinovo.cloudconductor.server.rest.shared;
 
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
 import de.cinovo.cloudconductor.api.interfaces.IAuth;
 import de.cinovo.cloudconductor.api.model.Authentication;
 import de.cinovo.cloudconductor.server.model.EJWTToken;
@@ -10,10 +16,6 @@ import de.cinovo.cloudconductor.server.security.AuthenticatedUserWithToken;
 import de.cinovo.cloudconductor.server.security.TokenHandler;
 import de.taimos.dvalin.jaxrs.JaxRsComponent;
 import de.taimos.restutils.RESTAssert;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
 
 /**
  * Copyright 2017 Cinovo AG<br>
@@ -23,16 +25,18 @@ import javax.ws.rs.core.Response;
  */
 @JaxRsComponent
 public class AuthImpl implements IAuth {
-
+	
 	@Autowired
 	private AuthHandler authHandler;
 	@Autowired
 	private TokenHandler tokenHandler;
-
+	
+	
 	@Override
+	@Transactional
 	public String auth(Authentication auth) {
 		RESTAssert.assertNotNull(auth);
-		if(auth.getToken() == null || auth.getToken().isEmpty()) {
+		if ((auth.getToken() == null) || auth.getToken().isEmpty()) {
 			RESTAssert.assertNotEmpty(auth.getUsername());
 			RESTAssert.assertNotEmpty(auth.getPassword());
 			EUser user = this.authHandler.getUser(auth.getUsername(), auth.getPassword());
@@ -42,24 +46,34 @@ public class AuthImpl implements IAuth {
 		EUser user = this.authHandler.getUser(auth.getToken());
 		return this.getToken(user, AuthType.AGENT, auth.getToken());
 	}
-
+	
 	@Override
+	@Transactional
 	public void logout() {
 		AuthenticatedUserWithToken currentAuthenticatedUser = this.authHandler.getCurrentAuthenticatedUser();
-		if(currentAuthenticatedUser != null) {
+		if (currentAuthenticatedUser != null) {
 			this.tokenHandler.revokeJWTToken(currentAuthenticatedUser.getToken());
 		} else {
 			EUser currentUser = this.authHandler.getCurrentUser();
 			this.tokenHandler.revokeJWTTokens(currentUser);
 		}
 	}
-
+	
+	@Override
+	@Transactional
+	public String refresh(String token) {
+		RESTAssert.assertNotEmpty(token);
+		EUser user = this.authHandler.getUser(token);
+		this.logout();
+		return this.getToken(user, AuthType.PERSON, token);
+	}
+	
 	private String getToken(EUser user, AuthType type, String refToken) {
-		if(user == null) {
+		if (user == null) {
 			throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED).build());
 		}
 		EJWTToken ejwtToken = this.tokenHandler.generateJWTToken(user, type, refToken);
-		if(ejwtToken == null) {
+		if (ejwtToken == null) {
 			throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED).build());
 		}
 		return ejwtToken.getToken();
--- src/main/java/de/cinovo/cloudconductor/server/security/AuthHandler.java
+++ src/main/java/de/cinovo/cloudconductor/server/security/AuthHandler.java
@@ -1,13 +1,14 @@
 package de.cinovo.cloudconductor.server.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import de.cinovo.cloudconductor.server.dao.IAuthTokenDAO;
 import de.cinovo.cloudconductor.server.dao.IUserDAO;
 import de.cinovo.cloudconductor.server.model.EAuthToken;
 import de.cinovo.cloudconductor.server.model.EUser;
 import de.taimos.dvalin.jaxrs.context.DvalinRSContext;
 import de.taimos.dvalin.jaxrs.security.IUser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 /**
  * Copyright 2017 Cinovo AG<br>
@@ -17,36 +18,37 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AuthHandler {
-
+	
 	@Autowired
 	private IUserDAO userDAO;
 	@Autowired
 	private DvalinRSContext rsContext;
 	@Autowired
 	private IAuthTokenDAO authTokenDAO;
-
+	
+	
 	/**
 	 * @return the current logged in user
 	 */
 	public EUser getCurrentUser() {
 		IUser currentUser = this.rsContext.getCurrentUser();
-		if(currentUser == null) {
+		if (currentUser == null) {
 			return null;
 		}
 		return this.userDAO.findByLoginName(currentUser.getUsername());
 	}
-
+	
 	/**
 	 * @return the current authenticated user with his token
 	 */
 	public AuthenticatedUserWithToken getCurrentAuthenticatedUser() {
 		IUser currentUser = this.rsContext.getCurrentUser();
-		if(currentUser == null || !(currentUser instanceof AuthenticatedUserWithToken)) {
+		if ((currentUser == null) || !(currentUser instanceof AuthenticatedUserWithToken)) {
 			return null;
 		}
 		return (AuthenticatedUserWithToken) currentUser;
 	}
-
+	
 	/**
 	 * @param username the username
 	 * @param password the passwort
@@ -54,22 +56,22 @@ public class AuthHandler {
 	 */
 	public EUser getUser(String username, String password) {
 		EUser user = this.userDAO.findByLoginName(username);
-		if(user == null) {
+		if (user == null) {
 			return null;
 		}
-		if(user.getPassword() == null || !user.getPassword().validate(password)) {
+		if ((user.getPassword() == null) || !user.getPassword().validate(password)) {
 			return null;
 		}
 		return user;
 	}
-
+	
 	/**
 	 * @param token the authentication token
 	 * @return the user
 	 */
 	public EUser getUser(String token) {
 		EAuthToken authToken = this.authTokenDAO.findByToken(token);
-		if(authToken == null || authToken.getUser() == null) {
+		if ((authToken == null) || (authToken.getUser() == null)) {
 			return null;
 		}
 		return authToken.getUser();
