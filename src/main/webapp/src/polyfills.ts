a5168855d48e4af08cd51506d29cc6831bbe421b
Marc Weise <marc.weise@cinovo.de>
2022-02-23 16:57:24 +0100

    Update to angular 12

--- src/main/webapp/src/polyfills.ts
+++ src/main/webapp/src/polyfills.ts
@@ -18,14 +18,8 @@
 * BROWSER POLYFILLS
 */
 
-import 'core-js/es7/array';
-
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
- import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /**
  * Required to support Web Animations `@angular/animation`.
@@ -38,7 +32,7 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 
