e7ec1682d418feb0fa3f2efc373558eabdbae0fd
Marc Weise <marc.weise@cinovo.de>
2023-02-10 15:18:20 +0100

    Update maven dependencies to match dvalin v1.33

--- src/test/java/de/cinovo/cloudconductor/server/TestConfig.java
+++ src/test/java/de/cinovo/cloudconductor/server/TestConfig.java
@@ -1,5 +1,6 @@
 package de.cinovo.cloudconductor.server;
 
+import de.taimos.daemon.spring.Configuration;
 import de.taimos.daemon.spring.SpringDaemonTestRunner.RunnerConfig;
 
 /**
@@ -27,6 +28,22 @@ public class TestConfig extends RunnerConfig {
 		this.addProperty("jwtauth.issuer", "cloudconductor");
 		this.addProperty("jwtauth.secret", "4ED267FE5BBA826F6D2EE71A3A5EE491275051234F997B19068FECF9729FC2AC");
 		this.addProperty("indexTaskType", "none");
+		
+		// https://ossindex.sonatype.org/vulnerability/CVE-2022-41853?component-type=maven&component-name=org.hsqldb%2Fhsqldb&utm_source=ossindex-client&utm_medium=integration&utm_content=1.8.1
+		this.addProperty("hsqldb.method_class_names", "abc"); // TODO remove as soon as hsqldb can be updated
+	}
+	
+	@Override
+	protected void addProperty(String key, String value) {
+		// TODO remove this override as soon as dvalin SpringDaemonTestRunner allows additional spring profiles for tests
+		if (Configuration.PROFILES.equals(key)) {
+			if (!value.isEmpty()) {
+				value += ",";
+			}
+			value += "http";
+		}
+		System.setProperty(key, value);
+		super.addProperty(key, value);
 	}
 	
 	@Override
